e9e8dffde0938b4f8d57325c8e3e153672209aec
BeverlyAB <fullstackmachine@Richards-MacBook-Pro.local>
2019-03-04 20:53:41 -0800

    Pod + postPage

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'YOUC_Event_Page' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for YOUC_Event_Page
+  pod 'Parse'
+  pod 'AlamofireImage'
+  pod 'MessageInputBar'
+
+  target 'YOUC_Event_PageTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'YOUC_Event_PageUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ YOUC_Event_Page/Controllers/PostPageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PostPageViewController.swift
+//  YOUC_Event_Page
+//
+//  Created by Beverly Abadines on 3/4/19.
+//  Copyright © 2019 BeverlyAb. All rights reserved.
+//
+
+import UIKit
+
+class PostPageViewController: UIViewController {
+
+    @IBOutlet weak var eventNameField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var coverView: UIImageView!
+    @IBOutlet weak var tagField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
