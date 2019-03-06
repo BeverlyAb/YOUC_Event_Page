56f7dfcd9a27511fa3e719f5e68c7192ee87eb16
Hamzah <hermainhanif@dhcp-v126-183.mobile.uci.edu>
2019-03-06 09:02:04 -0800

    trying

---
+++ YOUC_Event_Page/GoingEventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoingEventsTableViewCell.swift
+//  YOUC_Event_Page
+//
+//  Created by Hermain Hanif on 3/6/19.
+//  Copyright © 2019 BeverlyAb. All rights reserved.
+//
+
+import UIKit
+
+class GoingEventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
