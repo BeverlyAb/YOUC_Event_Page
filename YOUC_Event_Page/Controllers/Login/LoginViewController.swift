806a8cb7e3a390fc468e05703ac63beff055caa8
Tejal <tpatel10167@gmail.com>
2019-03-07 17:53:59 -0800

    Commit

---
+++ YOUC_Event_Page/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  YOUC_Event_Page
+//
+//  Created by Tejal Patel on 3/1/19.
+//  Copyright © 2019 BeverlyAb. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var customView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        customView.backgroundColor = UIColor.white.withAlphaComponent(0.8)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    
+}
