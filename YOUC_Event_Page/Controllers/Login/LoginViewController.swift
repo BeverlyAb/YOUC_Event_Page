fa966d88b5967e3daffaf3c4863254d8fb8e8bc1
Tejal <tpatel10167@gmail.com>
2019-03-07 17:48:11 -0800

    Commit

--- YOUC_Event_Page/Controllers/Login/LoginViewController.swift
+++ YOUC_Event_Page/Controllers/Login/LoginViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var customView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.isHidden = true
+        customView.backgroundColor = UIColor.white.withAlphaComponent(0.8)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        self.navigationController?.navigationBar.isHidden = false
     }
-    */
 
 }
---
+++ YOUC_Event_Page/Controllers/Login/SignInViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SignInViewController.swift
+//  YOUC_Event_Page
+//
+//  Created by Tejal Patel on 3/6/19.
+//  Copyright © 2019 BeverlyAb. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignInViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Sign In"
+        
+        usernameField.delegate = self
+        passwordField.delegate = self
+        
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+
+        // Do any additional setup after loading the view.
+    }
+    override func willMove(toParent parent: UIViewController?)
+    {
+        if parent == nil
+        {
+            self.navigationController?.navigationBar.isHidden = true
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "signInSegue", sender: nil)
+            } else {
+                print("Error : \(error?.localizedDescription)")
+                let alertController = UIAlertController(title: "YOUC", message:
+                    "Username or Password is incorrect", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+}
