b42469b2de902b6d7bfe18a3e27381e4edc8d0a7
Hermain Hanif <hmhanif@uci.edu>
2019-03-11 12:39:06 -0700

    adding settings

---
+++ YOUC_Event_Page/Controllers/Profile/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  YOUC_Event_Page
+//
+//  Created by Hermain Hanif on 3/11/19.
+//  Copyright © 2019 BeverlyAb. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
