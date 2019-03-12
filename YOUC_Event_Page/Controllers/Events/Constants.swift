8d8898120c8a1f75674830f108b2d756eccc08dd
derek chang <derekchangc00@gmail.com>
2019-03-12 15:44:55 -0700

    added richards event page

---
+++ YOUC_Event_Page/Controllers/Events/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  YOUCEvent
+//
+//  Created by Richard Absin on 3/10/19.
+//  Copyright © 2019 richard.absin24@outlook.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let twitterBlue = UIColor(red: 29.0/255.0, green: 161.0/255.0, blue: 242.0/255.0, alpha: 1.0)
+}
+
+
