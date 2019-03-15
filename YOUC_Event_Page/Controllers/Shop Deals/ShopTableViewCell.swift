a845b54578d64da99f3f9d3bd3d301050244221f
Tejal <tpatel10167@gmail.com>
2019-03-15 00:05:52 -0700

    Logout button. fixed mistake

---
+++ YOUC_Event_Page/Controllers/Shop Deals/ShopTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShopTableViewCell.swift
+//  YOUC_Event_Page
+//
+//  Created by Tejal Patel on 3/15/19.
+//  Copyright © 2019 BeverlyAb. All rights reserved.
+//
+
+import UIKit
+
+class ShopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- YOUC_Event_Page/Controllers/Shop Deals/ShopViewController.swift
+++ YOUC_Event_Page/Controllers/Shop Deals/ShopViewController.swift
@@ -1,15 +1,17 @@
+
 //
-//  SettingsViewController.swift
+//  ShopViewController.swift
 //  YOUC_Event_Page
 //
-//  Created by Hermain Hanif on 3/11/19.
+//  Created by Tejal Patel on 3/15/19.
 //  Copyright © 2019 BeverlyAb. All rights reserved.
 //
 
 import UIKit
-import Parse
 
-class SettingsViewController: UIViewController {
+class ShopViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,12 +19,15 @@ class SettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    
-    @IBAction func onLogout(_ sender: Any) {
-        PFUser.logOut()
-        self.performSegue(withIdentifier: "unwindToViewController1", sender: self)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShopTableViewCell") as! ShopTableViewCell
+        return cell
+    }
+
     /*
     // MARK: - Navigation
 
